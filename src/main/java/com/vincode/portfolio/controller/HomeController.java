ed886a936dede24db389a4e7bdb4ccf25696a2ac
Crypto-V <verejanvasile92@gmail.com>
2023-06-16 13:12:30 +0100

    added index page

---
+++ src/main/java/com/vincode/portfolio/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.vincode.portfolio.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String renderIndexPage(){
+        return "index";
+    }
+}
